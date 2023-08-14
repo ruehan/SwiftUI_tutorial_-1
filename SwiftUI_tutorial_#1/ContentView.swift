80d26cda06b9d9c91df88b74e4a688267bf4f3d8
ruehan <ruehan98@gmail.com>
2023-08-14 21:03:50 +0900

    struct practice

--- SwiftUI_tutorial_#1/ContentView.swift
+++ SwiftUI_tutorial_#1/ContentView.swift
@@ -11,46 +11,32 @@ struct ContentView: View {
     var body: some View {
         
         HStack {
-            VStack {
-                Text("1!")
-                    .fontWeight(.bold)
-                    .font(.system(size: 60))
-                Text("2!")
-                    .fontWeight(.bold)
-                    .font(.system(size: 60))
-                Text("3!")
-                    .fontWeight(.bold)
-                    .font(.system(size: 60))
-            }
-            .background(Color.red)
-            VStack {
-                Text("4!")
-                    .fontWeight(.bold)
-                    .font(.system(size: 60))
-                Text("5!")
-                    .fontWeight(.bold)
-                    .font(.system(size: 60))
-                Text("6!")
-                    .fontWeight(.bold)
-                    .font(.system(size: 60))
-            }
-            .background(Color.red)
-            VStack {
-                Text("7!")
-                    .fontWeight(.bold)
-                    .font(.system(size: 60))
-                Text("8!")
-                    .fontWeight(.bold)
-                    .font(.system(size: 60))
-                Text("9!")
-                    .fontWeight(.bold)
-                    .font(.system(size: 60))
-            }
-            .background(Color.red)
+            
+            
+            MyVStackView()
+            MyVStackView()
+            MyVStackView()
         }
         .padding(10)
         .background(Color.yellow)
         
+    }
+}
+
+struct MyVStackView: View {
+    var body: some View {
+        VStack {
+            Text("1!")
+                .fontWeight(.bold)
+                .font(.system(size: 60))
+            Text("2!")
+                .fontWeight(.bold)
+                .font(.system(size: 60))
+            Text("3!")
+                .fontWeight(.bold)
+                .font(.system(size: 60))
+        }
+        .background(Color.red)
     }
 }
 
