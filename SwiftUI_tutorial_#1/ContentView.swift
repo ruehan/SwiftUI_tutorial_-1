fdb37da14bc9cc247cbed4c80ec1ccc3db8a7ac5
ruehan <ruehan98@gmail.com>
2023-08-14 21:00:28 +0900

    Practice VStack / HStack

--- SwiftUI_tutorial_#1/ContentView.swift
+++ SwiftUI_tutorial_#1/ContentView.swift
@@ -9,15 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("안녕하세요!")
-                .fontWeight(.bold)
-                .font(.largeTitle)
+        
+        HStack {
+            VStack {
+                Text("1!")
+                    .fontWeight(.bold)
+                    .font(.system(size: 60))
+                Text("2!")
+                    .fontWeight(.bold)
+                    .font(.system(size: 60))
+                Text("3!")
+                    .fontWeight(.bold)
+                    .font(.system(size: 60))
+            }
+            .background(Color.red)
+            VStack {
+                Text("4!")
+                    .fontWeight(.bold)
+                    .font(.system(size: 60))
+                Text("5!")
+                    .fontWeight(.bold)
+                    .font(.system(size: 60))
+                Text("6!")
+                    .fontWeight(.bold)
+                    .font(.system(size: 60))
+            }
+            .background(Color.red)
+            VStack {
+                Text("7!")
+                    .fontWeight(.bold)
+                    .font(.system(size: 60))
+                Text("8!")
+                    .fontWeight(.bold)
+                    .font(.system(size: 60))
+                Text("9!")
+                    .fontWeight(.bold)
+                    .font(.system(size: 60))
+            }
+            .background(Color.red)
         }
-        .padding()
+        .padding(10)
+        .background(Color.yellow)
+        
     }
 }
 
